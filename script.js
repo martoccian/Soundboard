a2f8923a40f24083fae13851c97bf41f5a95127a
martoccian <martoccian@vcu.edu>
2026-09-04 23:35:40 +0000

    commit delete files and add new files for soundboard project

--- script.js
+++
@@ -1,8 +0,0 @@
-const sounds = ['Applause', 'Boo', 'Meow', 'Bruh', 'Bwomp', 'Wrong'];
-
-sounds.forEach((sound) => {
-    const btn = document.createElement('button');
-    btn.classList.add('btn');
-    btn.innerText = sound;
-    document.getElementById('buttons').appendChild(btn);
-});
