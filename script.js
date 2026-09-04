479fcf18b3af8fc1bc32970b879b55d1c66571f9
martoccian <martoccian@vcu.edu>
2026-09-04 23:44:55 +0000

    Fix site layout for GitHub Pages

---
+++ script.js
@@ -0,0 +1,19 @@
+const sounds = ['Applause', 'Boo', 'Meow', 'Bruh', 'Bwomp', 'Wrong'];
+
+sounds.forEach((sound) => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+    btn.innerText = sound;
+    document.getElementById('buttons').appendChild(btn);
+});
+    btn.addEventListener('click', () => {
+        document.getElementById(sound).play();
+    });
+function stopSounds() {
+    sounds.forEach((sound) => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0;
+    });
+}
